9cc40bede1a3ca967fcf1810d73ffd8658260eac
Fumpledump <Fumpledump@gmail.com>
2025-04-11 00:08:55 -0400

    Narrative System and Test Scene Added

---
+++ Assets/Scripts/Yarn Scripts/NarrativeHandler.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NarrativeHandler : MonoBehaviour
+{
+    [Header("Dialogue Settings")]
+    [SerializeField] private Sprite[] characterPortraits;
+    [SerializeField] private string startNode = "";
+    [SerializeField] private bool startAutomatically;
+
+    [Header("Components")]
+    [SerializeField] private DialogueRunner dialogueRunner;
+    [SerializeField] private Image activeCharacterPortrait;
+     private void Awake()
+    {
+        AddYarnCommands();
+    }
+
+    private void Start()
+    {
+        if(startAutomatically)
+        {
+            StartDialogue(startNode);
+        }
+    }
+
+    public void StartDialogue(string node)
+    {
+        dialogueRunner.StartDialogue(node);
+    }
+
+    // Converts Functions to Yarn Commands
+    private void AddYarnCommands()
+    {
+        dialogueRunner.AddCommandHandler("YarnTest", () => YarnTest());
+        dialogueRunner.AddCommandHandler<int>("ChangeCharacterSprite", ChangeCharacterSprite);
+        dialogueRunner.AddCommandHandler<string>("LoadSceneByName", LoadSceneByName);
+    }
+
+    private void YarnTest()
+    {
+        Debug.Log("Test");
+    }
+
+    public void ChangeCharacterSprite(int index)
+    {
+        if (index >= 0 && index < characterPortraits.Length)
+        {
+            activeCharacterPortrait.gameObject.SetActive(true);
+            activeCharacterPortrait.sprite = characterPortraits[index];
+        }else{
+            activeCharacterPortrait.gameObject.SetActive(false);
+        }
+    }
+    private void LoadSceneByName(string sceneName) => SceneManager.LoadScene(sceneName);
+}
